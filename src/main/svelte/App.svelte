2a53b92efdfb524363db668c3299051bad27966f
CyberShrek <illych.jr2@gmail.com>
2025-09-16 18:42:49 +0300

    Ыуегз

--- src/main/svelte/App.svelte
+++ src/main/svelte/App.svelte
@@ -1,6 +1,15 @@
 <script lang="ts">
 
-    $: console.log(`Hello, I'm svelte!`);
+    import Loading from "./webapp-client/components/common/misc/Loading.svelte"
+
+    $: console.log(`Hello, I'm svelte!`)
+
 </script>
 
 <p>Hello world!</p>
+
+<Loading/>
+
+<Loading/>
+
+<Loading/>
