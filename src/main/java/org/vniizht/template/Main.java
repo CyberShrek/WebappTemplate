6476ca307c349e3d6271859f058f25b860363282
CyberShrek <illych.jr2@gmail.com>
2025-10-22 17:34:09 +0300

    Ыуегз

--- src/main/java/org/vniizht/template/Main.java
+++ src/main/java/org/vniizht/template/Main.java
@@ -1,6 +1,21 @@
 package org.vniizht.template;
 
-public class Main {
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+@WebListener
+public class Main implements ServletContextListener {
+
+    @Override
+    public void contextInitialized(ServletContextEvent servletContextEvent) {
+        Workbook workbook = new XSSFWorkbook();
+        System.out.println("POI is working!");
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
