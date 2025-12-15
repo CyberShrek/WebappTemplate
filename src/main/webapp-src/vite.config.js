028f6abdf9bd32b74b65ec850ca60a3cae6430d2
CyberShrek <illych.jr2@gmail.com>
2025-12-15 17:44:52 +0300

    Ыуегз

--- src/main/webapp-src/vite.config.js
+++ src/main/webapp-src/vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import sveltePreprocess from 'svelte-preprocess'
 
+const contextRoot = '/template'
+
 export default defineConfig({
     plugins: [svelte({
             preprocess: sveltePreprocess()
@@ -9,12 +11,26 @@ export default defineConfig({
     )],
     build: {
         outDir: '../webapp',
-        emptyOutDir: true
+        emptyOutDir: true,
+        rollupOptions: {
+            input: {
+                index: 'index.html',
+                about: 'about.html'
+            }
+        }
     },
     server: {
         proxy: {
-            '/api': 'http://localhost:8080'
+            '/api': {
+                target: 'http://localhost:8080',
+                rewrite: (path) => path.replace(/^\/api/, `${contextRoot}/api`),
+                changeOrigin: true
+            },
+            '/servicebank': {
+                target: 'http://localhost:8080',
+                changeOrigin: true
+            }
         }
     },
-    base: '/template'
+    base: contextRoot
 })
